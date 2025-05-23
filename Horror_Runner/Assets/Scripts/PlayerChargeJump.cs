c873e2d972da192525d88412320110cbb689c6c2
Banbaro <leon_baur@hotmail.de>
2025-05-23 21:13:30 +0200

    [Add] Add charged Jump

---
+++ Horror_Runner/Assets/Scripts/PlayerChargeJump.cs
@@ -0,0 +1,87 @@
+using StarterAssets;
+using UnityEngine;
+
+public class PlayerChargeJump : MonoBehaviour
+{
+    // timeout deltatime
+    private float _jumpTimeoutDelta;
+    private float _fallTimeoutDelta;
+
+    private FirstPersonController _fpController;
+    private StarterAssetsInputs _input;
+    private GameObject _mainCamera;
+
+    private float _jumpLoad = 0.0f;
+    [SerializeField] private float jumpLoadMax = 20f;
+    [SerializeField] private float chargeTime = 20f;
+    
+    //private bool _isLoading = false;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _input = GetComponent<StarterAssetsInputs>();
+        _fpController = GetComponent<FirstPersonController>();
+        // reset our timeouts on start
+        _jumpTimeoutDelta = _fpController.JumpTimeout;
+        _fallTimeoutDelta = _fpController.FallTimeout;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Jump();
+    }
+
+    private void Jump()
+    {
+        if (_fpController.IsGrounded())
+        {
+            // reset the fall timeout timer
+            _fallTimeoutDelta = _fpController.FallTimeout;
+
+            // stop our velocity dropping infinitely when grounded
+            if (_fpController.VerticalVelocity < 0.0f)
+            {
+                _fpController.VerticalVelocity = -2f;
+            }
+
+            // Loading Jump
+            if (_input.jump && _jumpTimeoutDelta <= 0.0f)
+            {
+                // the square root of H * -2 * G = how much velocity needed to reach desired height
+                // _fpController.VerticalVelocity = Mathf.Sqrt(_fpController.JumpHeight * -2f * _fpController.Gravity);
+                if (_jumpLoad <= jumpLoadMax)
+                {
+                    _jumpLoad = _jumpLoad + (chargeTime * Time.deltaTime);
+                }
+            }
+
+            if (_input.jump == false && _jumpLoad >= 0.0f)
+            {
+                _fpController.VerticalVelocity = Mathf.Sqrt(_fpController.JumpHeight * -2f * _fpController.Gravity * _jumpLoad);
+                _jumpLoad = 0.0f;
+            }
+
+            // jump timeout
+            if (_jumpTimeoutDelta >= 0.0f)
+            {
+                _jumpTimeoutDelta -= Time.deltaTime;
+            }
+        }
+        else
+        {
+            // reset the jump timeout timer
+            _jumpTimeoutDelta = _fpController.JumpTimeout;
+
+            // fall timeout
+            if (_fallTimeoutDelta >= 0.0f)
+            {
+                _fallTimeoutDelta -= Time.deltaTime;
+            }
+
+            // if we are not grounded, do not jump
+            _input.jump = false;
+        }
+    }
+}
