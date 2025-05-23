a9e01f9991ed08a586f909d72a67425dc69ed255
Banbaro <leon_baur@hotmail.de>
2025-05-24 00:29:11 +0200

    [Add] Halbe fetige Scheisse commiten, weil Kevin nervt

--- Horror_Runner/Assets/Scripts/GrappleHook.cs
+++ Horror_Runner/Assets/Scripts/GrappleHook.cs
@@ -1,15 +1,58 @@
+using StarterAssets;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GrappleHook : MonoBehaviour
 {
+    private CharacterController _characterController;
+    private FirstPersonController _firstPersonController;
+    private StarterAssetsInputs _input;
+    [SerializeField] private GameObject mainCamera;
+    [SerializeField] private LineRenderer lineRenderer;
+    [SerializeField] private float maxDistant = 50.0f;
+    [SerializeField] private float grappleSpeed = 50.0f;
+    private Vector3 _grapplePoint;
+    private Vector3 _grappleDirection;
+    private bool _isGrappling = false;
+    
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        _characterController = GetComponent<CharacterController>();
+        _firstPersonController = GetComponent<FirstPersonController>();
+        _input = GetComponent<StarterAssetsInputs>();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        if (_input.jump)
+        {
+            if (Physics.Raycast(mainCamera.transform.position, mainCamera.transform.forward, out var hit, maxDistant))
+            {
+                _grapplePoint = hit.point;
+                _grappleDirection = _grapplePoint - transform.position;
+                _isGrappling = true;
+            }
+        }
+        
+        if (_isGrappling)
+        {
+            _characterController.Move(_grappleDirection.normalized * (Time.deltaTime * grappleSpeed));
+            _input.move = Vector2.zero;
+            _firstPersonController.VerticalVelocity = 0.0f;
+            if ((_grapplePoint - transform.position).magnitude <= 3)
+            {
+                _isGrappling = false;
+                lineRenderer.enabled = false;
+                _grapplePoint = Vector3.zero;
+            }
+        }
+    }
+
+    void FixedUpdate()
     {
         
     }
