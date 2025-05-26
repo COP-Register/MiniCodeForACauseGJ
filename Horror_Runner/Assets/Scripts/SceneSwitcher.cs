73dcb9d9895591714c8414bf62f04e26e2bc819f
Kevin <kleipert@web.de>
2025-05-26 04:04:28 +0200

    [UPDATE] scene chaos

--- Horror_Runner/Assets/Scripts/SceneSwitcher.cs
+++ Horror_Runner/Assets/Scripts/SceneSwitcher.cs
@@ -8,8 +8,13 @@ public class SceneSwitcher : MonoBehaviour
 {
     public float DelayForNextScene = 5f;
     public bool IsFillerScene;
+#if UNITY_EDITOR
     public SceneAsset _currentScene;
     public SceneAsset _nextScene;
+#endif
+
+    private string _currentSceneName;
+    private string _nextSceneName;
 
 
     private void Start()
@@ -17,9 +22,22 @@ public class SceneSwitcher : MonoBehaviour
         if(IsFillerScene) LoadNextScene();
     }
 
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        if (_currentScene != null && _nextScene != null)
+        {
+            string pathCurrent = AssetDatabase.GetAssetPath(_currentScene);
+            string pathNext = AssetDatabase.GetAssetPath(_nextScene);
+            _currentSceneName = System.IO.Path.GetFileNameWithoutExtension(pathCurrent);
+            _nextSceneName = System.IO.Path.GetFileNameWithoutExtension(pathNext);
+        }
+    }
+#endif
+
     public void ReloadCurrentScene()
     {
-        SceneManager.LoadScene(_currentScene.name);
+        SceneManager.LoadScene(_currentSceneName);
     }
 
     public void LoadNextScene()
@@ -30,6 +48,6 @@ public class SceneSwitcher : MonoBehaviour
     IEnumerator TriggerLoad()
     {
         yield return new WaitForSeconds(DelayForNextScene);
-        SceneManager.LoadScene(_nextScene.name);
+        SceneManager.LoadScene(_nextSceneName);
     }
 }
