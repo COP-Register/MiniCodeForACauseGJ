aabe642dc0659138dc924d77e55ef7831fa83076
Kevin <kleipert@web.de>
2025-05-26 04:17:28 +0200

    [FIX] unity editor remove

--- Horror_Runner/Assets/Scripts/SceneSwitcher.cs
+++ Horror_Runner/Assets/Scripts/SceneSwitcher.cs
@@ -1,17 +1,12 @@
-using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEditor;
 
 public class SceneSwitcher : MonoBehaviour
 {
     public float DelayForNextScene = 5f;
     public bool IsFillerScene;
-#if UNITY_EDITOR
-    public SceneAsset _currentScene;
-    public SceneAsset _nextScene;
-#endif
+
 
     private string _currentSceneName;
     private string _nextSceneName;
@@ -19,21 +14,10 @@ public class SceneSwitcher : MonoBehaviour
 
     private void Start()
     {
+        _currentSceneName = SceneManager.GetActiveScene().name;
+        SetNextScene();
         if(IsFillerScene) LoadNextScene();
     }
-
-#if UNITY_EDITOR
-    private void OnValidate()
-    {
-        if (_currentScene != null && _nextScene != null)
-        {
-            string pathCurrent = AssetDatabase.GetAssetPath(_currentScene);
-            string pathNext = AssetDatabase.GetAssetPath(_nextScene);
-            _currentSceneName = System.IO.Path.GetFileNameWithoutExtension(pathCurrent);
-            _nextSceneName = System.IO.Path.GetFileNameWithoutExtension(pathNext);
-        }
-    }
-#endif
 
     public void ReloadCurrentScene()
     {
@@ -49,5 +33,39 @@ public class SceneSwitcher : MonoBehaviour
     {
         yield return new WaitForSeconds(DelayForNextScene);
         SceneManager.LoadScene(_nextSceneName);
+    }
+
+    private void SetNextScene()
+    {
+        switch (_currentSceneName)
+        {
+            case "Level_0_Titel":
+                _nextSceneName = "Level_0";
+                break;
+            case "Level_0":
+                _nextSceneName = "Level_1_Titel";
+                break;
+            case "Level_1_Titel":
+                _nextSceneName = "Level_1";
+                break;
+            case "Level_1":
+                _nextSceneName = "Level_2_Titel";
+                break;
+            case "Level_2_Titel":
+                _nextSceneName = "Level_2";
+                break;
+            case "Level_2":
+                _nextSceneName = "Level_3_Titel";
+                break;
+            case "Level_3_Titel":
+                _nextSceneName = "Level_3";
+                break;
+            case "Level_3":
+                _nextSceneName = "Level_3_End";
+                break;
+            case "Level_3_End":
+                _nextSceneName = "Level_3_Thank";
+                break;
+        }
     }
 }
